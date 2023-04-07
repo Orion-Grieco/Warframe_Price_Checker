b6bd89a9321752bb205ab67db82016c06014ff49
Orion Grieco <Orion.grieco@Gmail.com>
2023-04-07 15:22:32 -0400

    updated code like

--- warframe.py
+++ warframe.py
@@ -1,35 +1,47 @@
 # Credits to Pojomi for search function
 
-import urllib.parse as parse
-import urllib.request
 from urllib.request import urlopen as req_url
 import json
-import string
-import webbrowser
 
-filename = "list.txt"
 
+filename = "warframe-market-item-names.txt"
 # Search API for most recent average price scrape
 def search(target):
-	
-	#print("Searching for " + string.capwords(target) + "...")
-	main_url = req_url('https://api.warframe.market/v1/items/' + target.replace(' ', '_').lower() + '/statistics')
-	
-	data = main_url.read()
-	parsed = json.loads(data)
-	parsed_data = parsed['payload']['statistics_live']['48hours'][-1]['min_price']
-	date_time = parsed['payload']['statistics_live']['48hours'][-1]['datetime']
 
-	return(parsed_data, date_time)
+    # print("Searching for " + string.capwords(target) + "...")
+    main_url = req_url(
+        "https://api.warframe.market/v1/items/"
+        + target.replace(" ", "_").lower()
+        + "/statistics"
+    )
+    # time.sleep(5)
+
+    data = main_url.read()
+    # time.sleep(5)
+    parsed = json.loads(data)
+    parsed_data = parsed["payload"]["statistics_live"]["48hours"][-1]["min_price"]
+    date_time = parsed["payload"]["statistics_live"]["48hours"][-1]["datetime"]
+
+    return (parsed_data, date_time)
 
 
 def read_list(filename):
-	with open (filename, "r") as infile:
-		data = infile.readlines()
-		for line in data:
-			price, time = search(line.strip())
-			print(f"{line.strip()}: {price} Platinum")
-			
+    with open(filename, "r") as infile:
+        data = infile.readlines()
+        for line in data:
+            price, time = search(line.strip())
+            print(f"{line.strip()}: {price} Platinum")
+            # -- Returning the price data along with the time at which the pricing data was recorded
+            # -- (This works iteratively per individual item)
+            # print(f"{line.strip()}: {price} Platinum -- [Time of evaluation: {time}]")
+
 
 read_list(filename)
 
+# -- Items without pricing data
+# Artillery Battery Scene
+# Ostron Chest Guard
+# Meso T6 Relic
+# Meso V8 Relic
+# Neo A7 Relic
+# Neo D6 Relic
